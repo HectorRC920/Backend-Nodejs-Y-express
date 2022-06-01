906662a8b92215876fcb18b8c3497e38d1fd7ac0
HectorRC920 <hector.rubio.920@gmail.com>
2022-06-01 16:21:58 -0600

    cors

--- app/server/index.js
+++ app/server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const routerApi = require('../routes')
+const cors = require('cors')
 const app = express()
 const port = '5040'
 var bodyParser = require('body-parser')
 const {logErrors, errorHandler, boomErrorHandler} = require('../middlewares/error.handler')
-
-
+const whiteList = ['http://localhost:3000']
+const options = {
+    origin: (origin, callback) => {
+        if(whiteList.includes(origin)){
+            callback(null, true)
+        } else {
+            callback(new Error('no permitido'))
+        }
+    }
+}
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
