091659ac4656c47c82fe7e5d76b2c0e3abb7b022
HectorRC920 <hector.rubio.920@gmail.com>
2022-06-11 16:09:13 -0600

    fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,7 @@ function routerApi(app) {
     app.use('/api/v1', router);
     router.use('/products', productsRouter);
     router.use('/categories', categoriesRouter);
+    router.use('/tasks', categoriesRouter);
 }
 
 module.exports = routerApi
